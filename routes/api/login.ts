91056bad4de254468ccbef1f306d643dd799efc8
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2022-11-19 07:00:34 +0000

    feat(auth): Implement basic authentication mechanism.
    For reference, see https://deno.com/blog/setup-auth-with-fresh

---
+++ routes/api/login.ts
@@ -0,0 +1,27 @@
+import { Handlers } from "$fresh/server.ts";
+import { setCookie } from "std/http/cookie.ts";
+
+export const handler: Handlers = {
+  async POST(req) {
+    const url = new URL(req.url);
+    const form = await req.formData();
+
+    if (form.get("username") !== "deno" || form.get("password") !== "land") {
+      return new Response(null, { status: 403 });
+    }
+
+    const headers = new Headers();
+    setCookie(headers, {
+      name: "auth",
+      value: "bar",
+      maxAge: 120,
+      sameSite: "Lax",
+      // Commented coz it doesn't work with Github Codespaces.
+      //   domain: url.hostname,
+      path: "/",
+      secure: true,
+    });
+    headers.set("location", "/");
+    return new Response(null, { status: 303, headers });
+  },
+};
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,31 @@
+import type { Handlers, PageProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
+import { getCookies } from "std/http/cookie.ts";
+
 import Counter from "../islands/Counter.tsx";
 
-export default function Home() {
+interface Data {
+  isAllowed: boolean;
+}
+
+export const handler: Handlers = {
+  GET(req, ctx) {
+    const cookies = getCookies(req.headers);
+    return ctx.render({ isAllowed: cookies.auth === "bar" });
+  },
+};
+
+function Login() {
+  return (
+    <form method="post" action="/api/login">
+      <input type="text" name="username" />
+      <input type="password" name="password" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default function Home({ data }: PageProps<Data>) {
   return (
     <>
       <Head>
@@ -14,9 +38,17 @@ export default function Home() {
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
+          Welcome to `fresh`. Try updating this message in the
+          ./routes/index.tsx file, and refresh.
         </p>
+        <pre>{JSON.stringify(data)}</pre>
+        {data.isAllowed ? (
+          <p>
+            You are logged in. <a href="/logout">Logout</a>
+          </p>
+        ) : (
+          <Login />
+        )}
         <Counter start={3} />
       </div>
     </>

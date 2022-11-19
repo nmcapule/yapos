2cbad3c93e8b7d5a3457fe784b9a275fa86c376f
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2022-11-19 08:55:39 +0000

    feat(backend): Include backend code and move frontend code in its own root folder.

---
+++ scripts/setup_backend.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pushd $(pwd) >> /dev/null
+
+readonly SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+readonly BACKEND_DIR=${SCRIPT_DIR}/../backend
+
+readonly REMOTEPKG=https://github.com/pocketbase/pocketbase/releases/download/v0.8.0/pocketbase_0.8.0_linux_amd64.zip
+readonly LOCALPKG=${BACKEND_DIR}/pocketbase.zip
+readonly POCKETBASE=${BACKEND_DIR}/pocketbase
+
+function setup_pocketbase() {
+    mkdir -p ${BACKEND_DIR}
+    wget ${REMOTEPKG} -O ${LOCALPKG}
+    unzip ${LOCALPKG} -d ${BACKEND_DIR}
+}
+
+test -f ${POCKETBASE} || setup_pocketbase
+
+cd ${BACKEND_DIR}
+${POCKETBASE} serve --http 0.0.0.0:8090
+
+popd >> /dev/null

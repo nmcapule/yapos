6f9284ad5d40f5fe3b1b03208829b50642a5522d
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2022-11-19 12:30:09 +0000

    refactor: Remove unused variable in logout.

--- frontend/routes/account/logout.tsx
+++ frontend/routes/account/logout.tsx
@@ -2,15 +2,11 @@ import { Handlers } from "$fresh/server.ts";
 import { deleteCookie } from "std/http/cookie.ts";
 
 export const handler: Handlers = {
-  GET(req) {
-    const params = new URLSearchParams(req.headers.get("cookie")!);
-    console.log(params);
-
+  GET(_req) {
     const headers = new Headers();
     deleteCookie(headers, "pb_auth", { path: "/" });
     headers.set("location", "/");
 
-    console.log(headers);
     return new Response(null, { status: 302, headers });
   },
 };
